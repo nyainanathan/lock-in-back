2b2ec951db98a63bd3fef952732f41aaabcda51f
Ny Aina Nathan <nyaina.nathan.1@gmail.com>
2026-02-02 16:47:32 +0300

    chore: custom user details service class

---
+++ src/main/java/com/nathan/lock_in/user/CustomUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.nathan.lock_in.user;
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@AllArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        Users user = userRepository.findByEmail(email);
+
+        if(user == null){
+            throw new UsernameNotFoundException("User with email " + email + " not found.");
+        }
+
+        return new CustomUserDetails(user);
+    }
+}
