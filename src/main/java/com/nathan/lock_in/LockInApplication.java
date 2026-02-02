78c5deefdab6068b76b3fbcda59492a87af77cd7
Ny Aina Nathan <nyaina.nathan.1@gmail.com>
2026-02-02 14:51:00 +0300

    chore: project init

---
+++ src/main/java/com/nathan/lock_in/LockInApplication.java
@@ -0,0 +1,13 @@
+package com.nathan.lock_in;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LockInApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LockInApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nathan/lock_in/LockInApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nathan.lock_in;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LockInApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
